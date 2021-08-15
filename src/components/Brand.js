691e7792554f40e95f6b256e75a873ebfdedfbfc
slicky1 <okunonke@gmail.com>
2021-08-15 12:22:58 -0400

    frontend toggle Update

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -2,11 +2,16 @@ import React, {useState} from 'react'
 
 export default function Brand({brand, deleteBrand, updateBrand}) {
     const [newBrands, setNewBrand] = useState({...brand})
+    const [editMode, setEditMode] = useState(false)
 
     function handleChange(e){
         const updateValue = {...newBrands}
         updateValue[e.target.name] = e.target.value
         setNewBrand({...updateValue})
+    }
+
+    function toggleEdit(){
+        setEditMode(!editMode)
     }
 
     function handleUpdate(e){
@@ -18,12 +23,22 @@ export default function Brand({brand, deleteBrand, updateBrand}) {
             <br />
             <p>{brand.name}</p>
 
-                <button onClick = {() => deleteBrand(brand)}>Delete Brand </button>
+            {
+                editMode &&
 
-                <form onSubmit ={handleUpdate}>
-                <input name ="name" value={newBrands.name} onChange={handleChange} />
-                <button type="submit">Update Brand</button>
-                </form>
+                <>
+                  <button onClick = {() => deleteBrand(brand)}>Delete Brand </button>
+
+                  <form onSubmit ={handleUpdate}>
+                  <input name ="name" value={newBrands.name} onChange={handleChange} />
+                  <button type="submit">Update Brand</button>
+                  </form>
+
+                </>
+            }
+
+            <button onClick = {toggleEdit}> Toggle Mode</button>
+
             <br />
         </div>
     )
